388e9bc2af2a3bdc4d9a1f1231ba27d19f77d752
Jorge Gonçalves <jrcgonc@gmail.com>
2021-02-08 15:46:51 +0000

    Export EventManager

--- lib/sip_ua.dart
+++ lib/sip_ua.dart
@@ -2,6 +2,7 @@
 export 'src/Config.dart';
 // export 'src/UA.dart';
 export 'src/enum_helper.dart';
+export 'src/event_manager/event_manager.dart';
 export 'src/logger.dart';
 export 'src/sip_ua_helper.dart';
 export 'src/transports/websocket_interface.dart';
