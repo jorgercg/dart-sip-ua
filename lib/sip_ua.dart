9e4a28087f409b975395b5facd00dd48247cf081
Jorge Gonçalves <jrcgonc@gmail.com>
2021-02-08 15:44:34 +0000

    progressing

--- lib/sip_ua.dart
+++ lib/sip_ua.dart
@@ -1,6 +1,6 @@
 /// only expose the bare minimum of internals required
 export 'src/Config.dart';
-export 'src/UA.dart';
+// export 'src/UA.dart';
 export 'src/enum_helper.dart';
 export 'src/logger.dart';
 export 'src/sip_ua_helper.dart';
